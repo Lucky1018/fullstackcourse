ff86f90410024f73383e96ebb46713ac98c9075c
Luc GUIGUEMDE <luc.guiguemde@it-centrex.com>
2023-05-16 17:16:53 +0000

    Commit exercice courseinfo part1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,56 @@
+const Header = (props) => {
+    return <h1>{props.course}</h1>
+}
+const Part=(props)=>{
+    return( <p>{props.partnumber} {props.exercisesnumber}</p>
+    )
+}
+
+const Content= (props) => {
+
+    return(
+       <div>
+           <Part partnumber={props.parts[0].name} exercisesnumber={props.parts[0].exercises}/>
+           <Part partnumber={props.parts[1].name} exercisesnumber={props.parts[1].exercises}/>
+           <Part partnumber={props.parts[2].name} exercisesnumber={props.parts[2].exercises}/>
+
+
+       </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercices {props.parts[0].exercises + props.parts[1].exercises+ props.parts[2].exercises}</p>
+    )
+}
+
+const App=()=> {
+  const course={
+      name:'Half Stack application development',
+      parts:[
+          {
+              name:'Fundamentals of Rect',
+              exercises:10
+          },
+          {
+              name:'Using props to pass data',
+              exercises:7
+          },
+          {
+              name:'State of a component',
+              exercises:14
+          }
+          ]
+  }
+
+  return(
+      <div>
+          <Header course={course.name}/>
+          <Content parts={course.parts} />
+          <Total parts={course.parts}/>
+      </div>
+  )
+}
+
+export default App;
